39c5fbf83bf6e96d2bb4c36ed8e8b06e8fc1e2bd
Gehongyan <gehongyan1996@126.com>
2024-10-15 22:29:54 +0800

    Add remarks to StartupCacheFetchData explaining fetch behavior

--- src/QQBot.Net.WebSocket/StartupCacheFetchData.cs
+++ src/QQBot.Net.WebSocket/StartupCacheFetchData.cs
@@ -14,16 +14,35 @@ public enum StartupCacheFetchData
     /// <summary>
     ///     加载频道角色列表。
     /// </summary>
+    /// <remarks>
+    ///     角色列表会在 Bot 启动时优先下载，若 <see cref="QQBot.WebSocket.QQBotSocketConfig.StartupCacheFetchMode"/>
+    ///     设置为 <see cref="QQBot.WebSocket.StartupCacheFetchMode.Synchronous"/>，
+    ///     <see cref="QQBot.WebSocket.QQBotSocketClient.Ready"/> 事件引发时角色列表应已经下载完成。
+    ///     调用 <see cref="QQBot.WebSocket.SocketGuild.UpdateAsync(QQBot.RequestOptions)"/> 时也会更新角色列表。
+    /// </remarks>
     Roles = 1 << 0,
 
     /// <summary>
     ///     加载子频道列表。
     /// </summary>
+    /// <remarks>
+    ///     子频道列表会在 Bot 启动时优先下载，若 <see cref="QQBot.WebSocket.QQBotSocketConfig.StartupCacheFetchMode"/>
+    ///     设置为 <see cref="QQBot.WebSocket.StartupCacheFetchMode.Synchronous"/>，
+    ///     <see cref="QQBot.WebSocket.QQBotSocketClient.Ready"/> 事件引发时子频道列表应已经下载完成。
+    ///     调用 <see cref="QQBot.WebSocket.SocketGuild.UpdateAsync(QQBot.RequestOptions)"/> 时也会更新角色列表。
+    /// </remarks>
     Channels = 1 << 1,
 
     /// <summary>
     ///     加载频道用户列表。
     /// </summary>
+    /// <remarks>
+    ///     子频道列表会在 Bot 启动时推迟下载，即便 <see cref="QQBot.WebSocket.QQBotSocketConfig.StartupCacheFetchMode"/>
+    ///     设置为 <see cref="QQBot.WebSocket.StartupCacheFetchMode.Synchronous"/>，
+    ///     <see cref="QQBot.WebSocket.QQBotSocketClient.Ready"/> 事件引发时也无法保证频道用户列表下载完成。
+    ///     调用 <see cref="QQBot.WebSocket.SocketGuild.UpdateAsync(QQBot.RequestOptions)"/> 时不会更新频道用户列表。
+    ///     要获取频道用户列表，需要调用 <see cref="QQBot.WebSocket.SocketGuild.DownloadUsersAsync(QQBot.RequestOptions)"/>。
+    /// </remarks>
     GuildUsers = 1 << 2,
 
     /// <summary>
