e6c3ce883cf39edef7fcaf6f432778abea714c29
Gehongyan <gehongyan1996@126.com>
2026-03-07 16:17:15 +0800

    修复 GroupBotEvent 反序列化

--- src/QQBot.Net.WebSocket/API/Gateway/GroupBotEvent.cs
+++ src/QQBot.Net.WebSocket/API/Gateway/GroupBotEvent.cs
@@ -1,10 +1,12 @@
 using System.Text.Json.Serialization;
+using QQBot.Net.Converters;
 
 namespace QQBot.API.Gateway;
 
 internal class GroupBotEvent
 {
     [JsonPropertyName("group_openid")]
+    [GuidJsonConverter]
     public required Guid GroupOpenid { get; init; }
 
     [JsonPropertyName("op_member_openid")]
